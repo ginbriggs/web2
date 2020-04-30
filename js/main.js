eec1ce873bfdea6ce95e60d4cfa3c816f447d182
ginbriggs <55155662+ginbriggs@users.noreply.github.com>
2020-04-29 19:41:43 -0500

    module8 jquery

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+    $('#dismiss').click(function() {
+
+        $('#form-details').hide();
+
+
+    });
+   
+setTimeout(function() {
+    $('#jumbotron').hide();
+},7000);
+
+
+
+});
+
+
+
+
